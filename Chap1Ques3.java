fc981cd893cb1ac4b9fcf968a04ad8fd6e779dcc
alan-ellis <ellis.alanb@gmail.com>
2013-08-10 11:39:42 -0400

    revised

--- Chap1Ques3.java
+++ Chap1Ques3.java
@@ -1,45 +1,38 @@
-import java.util.Arrays;
-
 
 public class Chap1Ques3 {
-	
-	public static String sorted(String s){
-		
-		char[] sArray = s.toCharArray();
-		StringBuffer result = new StringBuffer();
-		
-		Arrays.sort(sArray);
-		
-		for (int i = 0; i < sArray.length; i++){
-			result.append(sArray[i]);
+
+	public static boolean permutation(String s1, String s2) {
+		if (s1.length() == s2.length()){
+			
+			String string = s1 + s2;
+			
+			char[] charArray = string.toCharArray();
+
+			int[] bucket = new int[256];
+
+			for(int i = 0; i < charArray.length; i++)
+				bucket[charArray[i]] += 1;
+
+			for(int i = 0; i < charArray.length; i++){
+				
+				if(bucket[charArray[i]] % 2 != 0)
+					return false;
+			}
+			
+			return true;
 		}
 		
-		String sortedString = result.toString();
-		
-		return sortedString;
-	}
-	
-	public static boolean permutation(String s1, String s2){
-		
-		if (s1.length() != s2.length())
-			return false;
-		
-		else if (sorted(s1).equals(sorted(s2)))
-			return true;
-		
-		else
 		return false;
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		String tester1 = "spine";
 		String tester2 = "penis";
 		String tester3 = "where";
 		String tester4 = "at";
-		
+
 		System.out.println(permutation(tester1, tester2));
 		System.out.println(permutation(tester2, tester3));
 		System.out.println(permutation(tester3, tester4));
-		System.out.println(1%2);
-		}
+	}
 }
