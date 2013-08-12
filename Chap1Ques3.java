031d2055cd2e3d928fa47c43782bb669e9f93dd2
alan-ellis <ellis.alanb@gmail.com>
2013-08-12 13:20:27 -0400

    final

--- Chap1Ques3.java
+++ Chap1Ques3.java
@@ -1,45 +1,43 @@
-import java.util.Arrays;
-
-
 public class Chap1Ques3 {
-	
-	public static String sorted(String s){
-		
-		char[] sArray = s.toCharArray();
-		StringBuffer result = new StringBuffer();
-		
-		Arrays.sort(sArray);
-		
-		for (int i = 0; i < sArray.length; i++){
-			result.append(sArray[i]);
-		}
-		
-		String sortedString = result.toString();
-		
-		return sortedString;
-	}
-	
-	public static boolean permutation(String s1, String s2){
-		
-		if (s1.length() != s2.length())
-			return false;
-		
-		else if (sorted(s1).equals(sorted(s2)))
+
+	public static boolean permutation(String s1, String s2) {
+		if (s1.length() == s2.length()) {
+
+			//String string = s1 + s2;
+
+			char[] charArray1 = s1.toCharArray();
+			char[] charArray2 = s2.toCharArray();
+
+			int[] bucket = new int[256];
+			
+			for(int i = 0; i < 256; i++)
+				bucket[i] = 0;
+
+			for (int i = 0; i < charArray1.length; i++)
+				bucket[charArray1[i]] ++;
+
+			for (int i = 0; i < charArray2.length; i++) {
+
+				if (bucket[charArray2[i]] == 0)
+					return false;
+				else
+					bucket[charArray2[i]] --;
+			}
+
 			return true;
-		
-		else
+		}
+
 		return false;
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		String tester1 = "spine";
 		String tester2 = "penis";
 		String tester3 = "where";
 		String tester4 = "at";
-		
+
 		System.out.println(permutation(tester1, tester2));
 		System.out.println(permutation(tester2, tester3));
 		System.out.println(permutation(tester3, tester4));
-		
-		}
+	}
 }
